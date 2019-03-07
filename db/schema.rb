c0a35bbf1ca7f6f94dc02df8ffc39386a1e79e56
m-mogi-ojs <m.mogi.ojs@gmail.com>
2019-03-07 23:47:10 +0900

    6-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190304131523) do
+ActiveRecord::Schema.define(version: 20190307143734) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
